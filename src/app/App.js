2256deedf7432381df40d71b42ffaade4cc67c00
Christian Lapinig <clapinig@adobe.com>
2022-04-20 00:16:09 -0700

    Add background color and header section

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="text-3xl font-bold underline">
-        Hello world!
+      <div className="container mx-auto p-2">
+        <header>
+          <h1 className="text-xl">Hi, World!</h1>
+          <h2 className="text-5xl font-bold pt-3">I'm Christian Lapinig.</h2>
+          <h2 className="text-5xl font-bold pt-4">I like to code.</h2>
+        </header>
       </div>
     </div>
   );
