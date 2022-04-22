a20c29e4ae8d128b86c43ad144068b410693a48c
Christian Lapinig <clapinig@adobe.com>
2022-04-22 00:14:15 -0700

    Added responsive design to header

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto p-2">
+      <div className="w-3/5 lg:mx-auto md:mx-auto p-5">
         <header>
           <h1 className="text-xl">Hi, World!</h1>
-          <h2 className="text-5xl font-bold pt-3">I'm Christian Lapinig.</h2>
-          <h2 className="text-5xl font-bold pt-4">I like to code.</h2>
+          <h2 className="text-2xl lg:text-5xl md:text-3xl font-bold pt-3">
+            I'm Christian Lapinig.
+          </h2>
+
+          <div className="header-intro">
+            <p className="pt-8">
+              I'm an aspiring Software Developer who likes to solve everyday problems
+              Currently, I'm a Technical Support Engineer focused on solving problems at
+              Adobe.
+            </p>
+          </div>
         </header>
+
       </div>
     </div>
   );
