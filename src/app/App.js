02c3ab88d483b6883d2e67350f36aed695fd2f09
Christian Lapinig <clapinig@adobe.com>
2022-05-01 22:36:18 -0700

    Make header text responsive

--- src/app/App.js
+++ src/app/App.js
@@ -5,13 +5,13 @@ function App() {
     <div className="App">
       <div className="w-full p-5 lg:w-3/5 lg:mx-auto md:mx-auto lg:p-10">
         <header>
-          <h1 className="text-xl">Hi, World!</h1>
-          <h2 className="text-2xl lg:text-5xl md:text-3xl font-bold pt-3">
+          <h1 className="text-xl lg:text-3xl md:text-2xl">Hi, World!</h1>
+          <h2 className="text-2xl lg:text-7xl md:text-3xl font-bold pt-3">
             I'm Christian Lapinig.
           </h2>
 
           <div className="header-intro">
-            <p className="pt-8">
+            <p className="pt-8 text-base lg:text-xl md:text-xl">
               I'm a Software Developer who likes to solve everyday problems with code.
               Currently, I'm a Technical Support Engineer focused on resolving customer issues at&nbsp;
               <a 
